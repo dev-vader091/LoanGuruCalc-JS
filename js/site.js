33e673cc8cd49d0d0052587de06ad31e4d767f3b
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-01-22 20:47:28 -0600

    added to calculation and display functions

--- js/site.js
+++ js/site.js
@@ -14,40 +14,90 @@ function getValues() {
 function calculateLoan(amount, term, rate) {
   let currentBalance = amount;
   // rate = rate / 1200;
-  let totalMonthlyPayment;
+  let totalMonthlyPayment = ((amount) * (rate / 1200)) / (1-(1 + rate / 1200) ** -term);
+  // let totalMonthlyPayment;
   let interestPayment;
   let principalPayment;
   let totalInterest = 0;
   
+  let loanArray = [];
   
-  for(i = 1; i <= term; i++ ){
-    totalMonthlyPayment = ((amount) * (rate / 1200)) / (1-(1 + rate / 1200) ** -term);
+  for(i = 1; currentBalance > 0 ; i++ ){
+    let currentMonth = i
     interestPayment = currentBalance * (rate / 1200);
+    if (totalMonthlyPayment > currentBalance) {
+      totalMonthlyPayment = currentBalance + interestPayment;
+    }
+    
     principalPayment = totalMonthlyPayment - interestPayment;
     totalInterest += interestPayment;
     currentBalance -= principalPayment;
+    let totalCost = amount + totalInterest;
 
-  }
 
-  let loan = {
-    month: term,
+    loanStats = {
+    // interestAmt: amount * rate,
+    month: currentMonth,
     payment: totalMonthlyPayment,
     principal: principalPayment,
-    interestPayment: interestPayment,
+    interest: interestPayment,
     interestTotal: totalInterest,
-    balance: currentBalance
+    balance: currentBalance,
+    totalPrincipal: amount,
+    total: totalCost
+
+    }
+
+    loanArray.push(loanStats)
+    
   }
 
-  displayLoanTable(loan);
+  // let loan = {
+  //   month: term,
+  //   payment: totalMonthlyPayment,
+  //   principal: principalPayment,
+  //   interest: interestPayment,
+  //   interestTotal: totalInterest,
+  //   balance: currentBalance
+  // }
+
+  displayLoanTable(loanArray);
   
 }
 
 // view function
-function displayLoanTable(loan) {
+function displayLoanTable(loanArray) {
   let loanTable = document.getElementById('calcTable');
+  const tableRowTemplate = document.getElementById('tableRowTemplate');
+  console.log(loanArray);
+  let currentMonth;
+
+  loanTable.innerHTML = '';
   // create table row from template
+  for(i = 0; i < loanArray.length; i++) {
+    let loanRow = document.importNode(tableRowTemplate.content, true);
+    currentMonth = loanArray[i];
+
+    let tableCells = loanRow.querySelectorAll('td');
+    console.log(loanRow);
+
+    tableCells[0].innerHTML = currentMonth.month;
+    tableCells[1].innerHTML = currentMonth.payment;
+    tableCells[2].innerHTML = currentMonth.principal;
+    tableCells[3].innerHTML = currentMonth.interest;
+    tableCells[4].innerHTML = currentMonth.interestTotal;
+    tableCells[5].innerHTML = currentMonth.balance;
+
+    loanTable.appendChild(loanRow);
+
+  }
+
+  document.getElementById('monthlyPayment').innerHTML = currentMonth.payment;
+  document.getElementById('totalPrincipalAmount'). innerHTML = currentMonth.totalPrincipal;
+  document.getElementById('totalInterestAmount'). innerHTML = currentMonth.interestTotal;
+  document.getElementById('totalCost'). innerHTML = currentMonth.total;
   // display table with calculation results
-  console.log(loan)
+  
 }
 
 // validation function
